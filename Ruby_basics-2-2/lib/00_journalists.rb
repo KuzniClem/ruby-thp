8802d1c2f9c7d6733201c904dd4e7c166a828161
Jin-12 <coutjules@free.fr>
2019-01-11 14:15:33 +0100

    2 firsts questions add

--- Ruby_basics-2-2/lib/00_journalists.rb
+++ Ruby_basics-2-2/lib/00_journalists.rb
@@ -2,4 +2,17 @@ local_dir = File.expand_path('../', __FILE__)
 $LOAD_PATH.unshift(local_dir)
 require "data.rb"
 
-puts @logins
+puts "Combien y a t'il de journalistes dans ce array ?"
+puts @logins.length
+
+puts "Combien d'handle contiennent un numero ?"
+puts 379 -  (@logins.select {
+  |log| log.each_char {
+    |char| case char
+    when ('0'..'9')
+      #puts log (affiche les handles valides. )
+      true
+      break
+    end
+  }
+}.length)
